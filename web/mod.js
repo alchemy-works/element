a8d73f7eb9f81686b5bfd72a401933565a3ca6c2
cbdyzj <cbdyzj@jianzhao.org>
2021-04-02 22:57:33 +0800

    Fix index html response

--- web/mod.js
+++ web/mod.js
@@ -1,8 +1,9 @@
 async function handleRequest(request) {
 
-    const index = new URL('index.html', import.meta.url)
-    const indexResponse = await fetch(index)
-    return new Response(indexResponse, {
+    const indexUrl = new URL('index.html', import.meta.url).toString()
+    const indexResponse = await fetch(indexUrl)
+    const indexText = await indexResponse.text()
+    return new Response(indexText, {
         headers: {
             'content-type': 'text/html; charset=UTF-8',
         },
