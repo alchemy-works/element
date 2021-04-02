e92a9f8e6235c9ab94c001bf8dd4c13fb65d661f
cbdyzj <cbdyzj@jianzhao.org>
2021-04-02 22:11:22 +0800

    Init

---
+++ src/test/java/alchemy/works/element/ElementApplicationTests.java
@@ -0,0 +1,13 @@
+package alchemy.works.element;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
